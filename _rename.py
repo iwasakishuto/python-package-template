8170a00865b3376fc3c2d67a4e66875ee944c5ac
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-08-18 00:54:22 +0900

    Minor Updates

--- _rename.py
+++ _rename.py
@@ -5,19 +5,26 @@ from pathlib import Path
 from _conf import *
 
 def rename(string):
-    re.sub(pattern=r"{{\s+(.+)\s+}}", repl=lambda m:print(f"Renamed {m.group(1)}");globals().get(m.group(1)), string=string)
+    return re.sub(pattern=r"{{\s+(.+)\s+}}", repl=lambda m:[print(f"\tRenamed {m.group(1)}"),globals().get(m.group(1))][-1], string=string)
+
+HERE = os.path.abspath(os.path.dirname(__file__))
 
 if __name__ == "__main__":
-    p = Path("")
-    os.rename("{{ MODULE_NAME }}", rename)
+    p = Path(HERE)
     for path in p.glob("**/*"):
+        rela_path = str(path.resolve().relative_to(HERE))
         if path.is_dir():
-            dirname = path.name
-            path.rename(dirname)
-            print(f"Rename DIR from {path.name} to {dirname}")
+            dirname = rename(rela_path)
+            if rela_path != dirname:
+                path.rename(dirname)
+                print(f"\033[32m[DIR]\033[0m renamed from {rela_path} to {dirname}")
         elif path.is_file():
-            print(f"FILE: {path.name}")
-            with path.open(mode="r") as f:
-                lines = [rename(e) for e in f.readlines()]
-            with path.open(mode="w") as f:
-                f.writelines(lines)
+            try:
+                with path.open(mode="r") as f:
+                    lines = f.readlines()
+                print(f"\033[34m[FILE]\033[0m: {rela_path}")
+                lines = [rename(e) for e in lines]
+                with path.open(mode="w") as f:
+                    f.writelines(lines)
+            except Exception as e:
+                pass
