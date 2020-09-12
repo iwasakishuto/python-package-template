86eb61a77e6595322152a6f015e5bf67fe884b4e
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-09-12 21:45:07 +0900

    Add KeyError

--- {{ MODULE_NAME }}/utils/_exceptions.py
+++ {{ MODULE_NAME }}/utils/_exceptions.py
@@ -6,3 +6,7 @@ class {{ PACKAGE_NAME_CODE }}ImprementationError(Exception):
     Warnings that developers will resolve. 
     Developers are now solving in a simple stupid way.
     """
+
+class KeyError(KeyError):
+    def __str__(self):
+        return ', '.join(self.args)
